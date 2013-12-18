4487a87a2a4a0e5a4b6d3e5c92d199c6eef1a287
Shashwat <thegreatshasha@gmail.com>
2013-12-18 19:42:33 +0530

    pushing

--- crawler.rb
+++ crawler.rb
@@ -8,6 +8,10 @@ class Object
     require 'cgi' unless defined?(CGI) && defined?(CGI::escape)
     "#{CGI.escape(key.to_param)}=#{CGI.escape(to_param.to_s)}"
   end
+
+	def to_param
+    to_s
+  end
 end
 
 class Hash
