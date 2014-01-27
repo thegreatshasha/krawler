c443be72a2e717cbaeb1c2d6d0bd4a3fbbe3b180
Shashwat <thegreatshasha@gmail.com>
2014-01-27 19:56:12 +0530

    Lets get highest priority movers first. Then rerun algorithm

--- crawler.rb
+++ crawler.rb
@@ -353,7 +353,7 @@ class Runner
 		@syncer = YelpSync.new({category: "movers", debug_level: 2})
 		#Phase 1
 		states = ["AK",  "AL",  "AR",  "AS",  "AZ",  "CA",  "CO",  "CT",  "DC",  "DE",  "FL",  "GA",  "GU",  "HI",  "IA",  "ID",  "IL",  "IN",  "KS",  "KY",  "LA",  "MA",  "MD",  "ME",  "MI",  "MN",  "MO",  "MP",  "MS",  "MT",  "NC",  "ND",  "NE",  "NH",  "NJ",  "NM",  "NV",  "NY",  "OH",  "OK",  "OR",  "PA",  "PR",  "RI",  "SC",  "SD",  "TN",  "TX",  "UM",  "UT",  "VA",  "VI",  "VT",  "WA",  "WI",  "WV",  "WY"]
-		states = ["CT", "IN", "HI"]#, "DA", "GC", "FL"]
+		#states = ["CT", "IN", "HI"]#, "DA", "GC", "FL"]
 
 		unless config[:cache]
 			syncer.write_state_links(states)
